51736e05e24137cc9bea59affa7621e0a421c143
biko-73 <57875905+biko-73@users.noreply.github.com>
2025-04-07 10:03:24 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='xklass/main'
 
-MY_IPK="enigma2-plugin-extensions-xklass_1.41_all.ipk"
-MY_DEB="enigma2-plugin-extensions-xklass_1.41_all.deb"
+MY_IPK="enigma2-plugin-extensions-xklass_1.45_all.ipk"
+MY_DEB="enigma2-plugin-extensions-xklass_1.45_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
