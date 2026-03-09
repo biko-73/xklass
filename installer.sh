2851b2f2637e287527ba1cf31e2d925892cf6839
biko-73 <57875905+biko-73@users.noreply.github.com>
2026-03-09 10:23:36 +0200

    Update package version to 1.78

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='xklass/main'
 
-MY_IPK="enigma2-plugin-extensions-xklass_1.76_all.ipk"
-MY_DEB="enigma2-plugin-extensions-xklass_1.76_all.deb"
+MY_IPK="enigma2-plugin-extensions-xklass_1.78_all.ipk"
+MY_DEB="enigma2-plugin-extensions-xklass_1.78_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
